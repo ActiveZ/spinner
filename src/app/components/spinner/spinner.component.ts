5dec73ef8f84f44e8f777d53fbe9f18a957022bc
ActiveZ <activez.git@gmail.com>
2022-08-07 04:39:14 +0200

    changement diametre

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,16 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
-// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-// import { CommonModule } from '@angular/common';
-// import { NgModule } from '@angular/core';
-// import { SharedModule } from 'src/app/commun/modules/shared.module';
-
 @Component({
   selector: 'spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss'],
 })
+
 export class SpinnerComponent implements OnInit {
   @Input() show: boolean = false;
+  
   constructor() {}
 
   ngOnInit(): void {}
